b474a166b3857098ecfcbaf3eb09fb2a0cc7818b
mdubravic83 <122885924+mdubravic83@users.noreply.github.com>
2026-02-06 10:51:20 +0100

    Added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname), {
+  maxAge: 0,
+  setHeaders: (res, filepath) => {
+    if (filepath.includes('/static/')) {
+      res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+    } else {
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    }
+  }
+}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
